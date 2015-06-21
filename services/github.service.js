76cf17e4e367221982018588b06f37af70889539
ángel <abr4xas@users.noreply.github.com>
2015-06-21 12:13:36 -0430

    Update github.service.js

--- services/github.service.js
+++ services/github.service.js
@@ -10,7 +10,8 @@
         .constant('gh',{
             BASE    : 'https://api.github.com',
             ORG     : '/orgs/ngVenezuela',
-            REPOS   : '/orgs/ngVenezuela/repos'
+            REPOS   : '/orgs/ngVenezuela/repos',
+            MEMBERS : '/orgs/ngVenezuela/members'
         })
         .factory('GitHub',githubService);
 
@@ -18,11 +19,13 @@
     githubService.$inject = ['$log','$http','gh'];
     function githubService ($log, $http,hg) {
         return {
-            organizacion : getOrganization;
+            organizacion : getOrganization,
+            repositorios : getRepos,
+            miembros     : getMembers
         };
         /**
         * @description realiza una petición a la API de Github
-        * solicitando la información de ngVenezuela como 
+        * solicitando la información de ngVenezuela como
         * oganización.
         */
         function getOrganization () {
@@ -37,7 +40,39 @@
                 .catch(function(response){
                     $log.error('Error organizacion');
                     return response;
-                });                
+                });
         }
-    }
+        /**
+        * @description realiza una petición a la API de Github
+        * solicitando la información de los repos de ngVenezuela.
+        */
+        function getRepos () {
+            var peticion = $http({
+                method : 'GET',
+                url : gh.BASE+gh.REPOS
+            });
+            return peticion
+                .then(function(response){
+                    return response.data;
+                })
+                .catch(function(response){
+                    $log.error('Error organizacion');
+                    return response;
+                });
+        }
+        function getMembers () {
+            var peticion = $http({
+                method : 'GET',
+                url : gh.BASE+gh.MEMBERS
+            });
+            return peticion
+                .then(function(response){
+                    return response.data;
+                })
+                .catch(function(response){
+                    $log.error('Error organizacion');
+                    return response;
+                });
+        }
+    } // Fin githubService
 })();
